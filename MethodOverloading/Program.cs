05e327b12693bb54f763d63946b44962ff38d88c
Colton Briggs <wolflink013@gmail.com>
2025-07-09 08:56:11 -0700

    Done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var result = Add(3.3,5.2,false);
+            Console.WriteLine(result);
+        }
+
+        private static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        private static double Add(double numOne, double numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        private static string Add(int numOne, int numTwo, bool isCash)
+        {
+            if (isCash)
+            {
+                int result = numOne + numTwo;
+                return result != 1 ? $"{result} dollars" : $"{result} dollar";
+            }
+            else
+            {
+                return $"{numOne + numTwo}";
+            }
+        }
+        private static string Add(double numOne, double numTwo, bool isCash)
+        {
+            if (isCash)
+            {
+                double result = numOne + numTwo;
+                double resultCash = Math.Truncate(result);
+                double decimalPart =  Math.Round(result - resultCash, 2);
+                string resultString = $"{resultCash} dollars and {decimalPart} cents";
+                return resultString;
+            }
+            else
+            {
+                return $"{numOne + numTwo}";
+            }
         }
     }
 }
